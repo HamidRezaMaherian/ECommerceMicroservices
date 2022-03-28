2c0b0e29a583119b5c9297eac7f65cbf3d0b49ee
Hamid.RM <hamidali.reza41@outlook.com>
2022-03-29 00:33:10 +0430

    initialized identity api project and tests

--- Services/Identity/Identity.API/Program.cs
+++ Services/Identity/Identity.API/Program.cs
@@ -1,25 +1,65 @@
-var builder = WebApplication.CreateBuilder(args);
+using Identity.API;
+using Identity.API.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	public static void Main(string[] args)
+	{
+
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+
+		builder.Services.AddControllers();
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddDbContext<ApplicationDbContext>(options =>
+		{
+			options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+		});
+		builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+					.AddDefaultTokenProviders()
+					.AddEntityFrameworkStores<ApplicationDbContext>();
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+
+		builder.Services.AddIdentityServer(options =>
+		{
+			options.Events.RaiseErrorEvents = true;
+			options.Events.RaiseInformationEvents = true;
+			options.Events.RaiseFailureEvents = true;
+			options.Events.RaiseSuccessEvents = true;
+			options.EmitStaticAudienceClaim = true;
+		})
+		//	.AddApiAuthorization<IdentityUser, ApplicationDbContext>(opt =>
+		//opt.SigningCredential = new SigningCredentials(
+		//	new SymmetricSecurityKey(Encoding.ASCII.GetBytes("KLJIWSFJLSJOGUWIOJKUWIOEL")), "SHA256")
+		//)
+		.AddInMemoryIdentityResources(Config.IdentityResources)
+		.AddTestUsers(Config.TestUsers.ToList())
+		.AddDeveloperSigningCredential()
+		.AddInMemoryApiScopes(Config.ApiScopes)
+		.AddInMemoryClients(Config.Clients);
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+		app.UseIdentityServer();
+
+		app.MapControllers();
+
+		app.Run();
+	}
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ Services/Identity/Tests/Identity.API.Tests/Integration/EndPointsTests.cs
@@ -0,0 +1,53 @@
+﻿using Identity.API.Data;
+using Identity.API.Tests.Utils;
+using IdentityModel.Client;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace Identity.API.Tests.Integration
+{
+	[TestFixture]
+	public class EndPointsTests
+	{
+		private HttpClient _appClient;
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			var dbName = "test_db";
+			_appClient = new TestingWebAppFactory<Program>(s =>
+			{
+				var dbContextConfiguration = s.SingleOrDefault(opt => opt.ServiceType == typeof(DbContextOptions<ApplicationDbContext>));
+				if (dbContextConfiguration != null)
+					s.Remove(dbContextConfiguration);
+				s.AddDbContextPool<ApplicationDbContext>(opt => opt.UseInMemoryDatabase(dbName));
+			}).CreateClient();
+		}
+		[OneTimeTearDown]
+		public void OneTimeTearDown()
+		{
+			_appClient.Dispose();
+		}
+		[Test]
+		public void DiscoveryEndpoint_ReturnOk()
+		{
+			var res = _appClient.GetDiscoveryDocumentAsync().Result;
+			Assert.AreEqual(HttpStatusCode.OK, res.HttpStatusCode);
+		}
+		[Test]
+		public void TokenEndpoint_GetToken()
+		{
+			var res = _appClient.RequestTokenAsync(new TokenRequest()
+			{
+				GrantType = "client_credentials",
+				Address = "/connect/token",
+				ClientId = "client",
+				ClientSecret = "secret",
+			}).Result;
+			Assert.AreEqual(HttpStatusCode.OK, res.HttpStatusCode);
+		}
+	}
+}
