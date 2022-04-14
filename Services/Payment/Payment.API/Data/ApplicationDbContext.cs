21eab012d64d72238bca3b448ecaa198925a65f7
Hamid.RM <hamidali.reza41@outlook.com>
2022-04-14 19:27:28 +0430

    added payment microservice and gateway configuration

---
+++ Services/Payment/Payment.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Payment.API.Data
+{
+	public class ApplicationDbContext
+	{
+	}
+}
---
+++ Services/Payment/Payment.API/Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.EntityFrameworkCore;
+using Parbad.Builder;
+using Parbad.Gateway.ParbadVirtual;
+using Parbad.Storage.EntityFrameworkCore.Builder;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+
+		builder.Services.AddControllers();
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+
+		builder.Services.AddParbad()
+			.ConfigureGateways(gtw =>
+			{
+				gtw.AddParbadVirtual()
+				.WithOptions(opt => opt.GatewayPath = "/paymentgtw");
+			})
+			.ConfigureStorage(stg =>
+			{
+				stg.UseEfCore(opt =>
+				{
+					var assemblyName = typeof(Program).Assembly.GetName().Name;
+					opt.ConfigureDbContext = db => db.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"), sql => sql.MigrationsAssembly(assemblyName));
+				});
+			})
+			.ConfigureHttpContext(opt=>opt.UseDefaultAspNetCore());
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+		if (app.Environment.IsDevelopment())
+		{
+			app.UseSwagger();
+			app.UseSwaggerUI();
+		}
+
+		app.UseHttpsRedirection();
+
+		app.UseAuthorization();
+
+		app.MapControllers();
+		app.UseParbadVirtualGatewayWhenDeveloping();
+		app.Run();
+	}
+}
