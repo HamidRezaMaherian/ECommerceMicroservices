217d0cda204735c05d95bb831000a77d6fba43c2
Hamid.RM <hamidali.reza41@outlook.com>
2022-04-17 22:09:45 +0430

    added apigateway test project

--- Gateways/APIGateway/Program.cs
+++ Gateways/APIGateway/Program.cs
@@ -5,7 +5,7 @@ public class Program
 	public static void Main(string[] args)
 	{
 		var builder = WebApplication.CreateBuilder(args);
-		builder.Configuration.AddJsonFile($"ocelt.json");
+		builder.Configuration.AddJsonFile($"ocelot.json");
 		builder.Services.AddOcelot();
 		var app = builder.Build();
 		app.UseOcelot().Wait();
---
+++ Gateways/Tests/APIGateway.Tests/Integration/EndPointsTests.cs
@@ -0,0 +1,31 @@
+﻿using APIGateway.API.Tests.Utils;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+
+namespace APIGateway.API.Tests.Integration
+{
+	[TestFixture]
+	public class EndPointsTests
+	{
+		private HttpClient _appClient;
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			_appClient = new TestingWebAppFactory<Program>().CreateClient();
+		}
+		[OneTimeTearDown]
+		public void OneTimeTearDown()
+		{
+			_appClient.Dispose();
+		}
+		[Test]
+		public void Initialized_ReturnTrue()
+		{
+			var res = _appClient.GetAsync("/home").Result;
+			res.EnsureSuccessStatusCode();
+		}
+	}
+}
