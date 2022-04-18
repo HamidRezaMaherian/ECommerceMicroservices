484b2a4ff3e6555999a421845ac661836420cee8
Hamid.RM <hamidali.reza41@outlook.com>
2022-04-18 16:13:17 +0430

    modified services ports

--- Gateways/APIGateway/Program.cs
+++ Gateways/APIGateway/Program.cs
@@ -1,12 +1,14 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
+
 public class Program
 {
 	public static void Main(string[] args)
 	{
 		var builder = WebApplication.CreateBuilder(args);
 		builder.Configuration.AddJsonFile($"ocelot.json");
-		builder.Services.AddOcelot();
+		builder.Services.AddOcelot().AddPolly();
 		var app = builder.Build();
 		app.UseOcelot().Wait();
 		app.Run();
