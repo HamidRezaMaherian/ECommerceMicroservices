81423333dd8ee27f6240710302ddc2c7839f0b8c
Hamid.RM <hamidali.reza41@outlook.com>
2022-04-20 14:11:19 +0430

    fixed consul configuration with api gateway

--- Gateways/APIGateway/Extensions.cs
+++ Gateways/APIGateway/Extensions.cs
@@ -4,13 +4,14 @@ public static class Extensions
 {
 	public static IServiceCollection ConfigureConsul(this IServiceCollection serviceCollection)
 	{
+		serviceCollection.AddTransient<IConsulClient, ConsulClient>();
+
 		using var services = serviceCollection.BuildServiceProvider();
 		var consulClient = services.GetService<IConsulClient>();
 		var configuration = services.GetService<IConfiguration>();
 
 		foreach (var item in Statics.Discoveries)
 		{
-			item.ID = $"{item.Name}:{Guid.NewGuid().ToString().Replace("-", "")[0]}";
 			consulClient?.Agent.ServiceDeregister(item.ID).Wait();
 			consulClient?.Agent.ServiceRegister(item).Wait();
 		};
--- Gateways/APIGateway/Statics.cs
+++ Gateways/APIGateway/Statics.cs
@@ -6,51 +6,59 @@ public static class Statics
 	{
 		new AgentServiceRegistration()
 		{
+			ID="basket:3",
 			Name="basket",
 			Port=5000,
-			Address="http://localhost"
+			Address="localhost"
 		},
 		new AgentServiceRegistration()
 		{
+			ID="discount:e",
 			Name="discount",
 			Port=5001,
-			Address="https://localhost"
+			Address="localhost"
 		},
 		new AgentServiceRegistration()
 		{
+			ID="identity:1",
 			Name="identity",
 			Port=5002,
-			Address="https://localhost"
+			Address="localhost",
 		},
 		new AgentServiceRegistration()
 		{
+			ID="inventory:c",
 			Name="inventory",
 			Port=5003,
-			Address="https://localhost"
+			Address="localhost"
 		},
 		new AgentServiceRegistration()
 		{
+			ID="order:3",
 			Name="order",
 			Port=5004,
-			Address="https://localhost"
+			Address="localhost"
 		},
 		new AgentServiceRegistration()
 		{
+			ID="payment:2",
 			Name="payment",
 			Port=5005,
-			Address="https://localhost"
+			Address="localhost"
 		},
 		new AgentServiceRegistration()
 		{
+			ID="product:3",
 			Name="product",
 			Port=5006,
-			Address="https://localhost"
+			Address="localhost"
 		},
 		new AgentServiceRegistration()
 		{
+			ID="ui:5",
 			Name="ui",
 			Port=5007,
-			Address="https://localhost"
+			Address="localhost"
 		},
 	};
 }
