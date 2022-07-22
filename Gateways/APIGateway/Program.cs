53e4b69b147ea0a0d2a8732d948c1114dcc64642
Hamid.RM <hamidali.reza41@outlook.com>
2022-07-22 14:39:47 +0430

    edit apiGateway and serviceDiscovery Configurations

--- Gateways/APIGateway/Program.cs
+++ Gateways/APIGateway/Program.cs
@@ -8,9 +8,11 @@ public class Program
 	public static void Main(string[] args)
 	{
 		var builder = WebApplication.CreateBuilder(args);
+		builder.Services.AddHealthChecks();
 		builder.Configuration.AddJsonFile($"ocelot.json");
 		builder.Services.AddOcelot().AddConsul().AddPolly();
 		var app = builder.Build();
+		app.UseHealthChecks("/health");
 		app.UseOcelot().Wait();
 		app.Run();
 	}
